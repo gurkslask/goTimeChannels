25a3c0da5634fb5c31be36f9de4fc818e256019f
Alexander Svensson <gurkslask@gmail.com>
2017-10-31 06:53:03 -0700

    restructured

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -13,38 +12,31 @@ import (
 const settingsname = "settings.json"
 
 func main() {
-	var tc timeChannel
-	tc.Name = "test"
-	tc.Timepoints.newtimePoint(1, 2, 2, 2, false)
-	err := tc.toJSON()
+	tcs := inittimeChannels()
+	tcs.newtimeChannel("test")
+	tcs.Tcs["test"].Timepoints.newtimePoint(1, 2, 2, 2, false)
+	tcs.Tcs["test"].Timepoints.newtimePoint(1, 3, 2, 2, false)
+	err := tcs.toJSON()
 	if err != nil {
-		log.Fatalf("Error at json %v", err)
+		log.Fatalf("%v", err)
 	}
-	err = fromJSON()
+
 }
 
-func fromJSON() error {
-	b, err := ioutil.ReadFile(settingsname)
-	if err != nil {
-		return err
-	}
-	fmt.Println(b)
-	var tc timeChannel
-	err = json.Unmarshal(b, &tc)
-	fmt.Println(tc)
-	if err != nil {
-		return err
-	}
-	return nil
+type timeChannels struct {
+	Tcs map[string]*timeChannel
 }
 
-type timeChannels []timeChannel
-
-func (tcs *timeChannels) newtimeChannel(weekDay, hour, minute, second int, state bool, name string) {
-	*tcs = append(*tcs, timeChannel{
-		Name: name,
-	})
+func inittimeChannels() *timeChannels {
+	return &timeChannels{
+		Tcs: make(map[string]*timeChannel),
+	}
 }
+
+func (tcs *timeChannels) newtimeChannel(name string) {
+	tcs.Tcs[name] = newtimeChannel(name)
+}
+
 func (tcs *timeChannels) fromJSON() error {
 	b, err := ioutil.ReadFile("settings.json")
 	if err != nil {
@@ -75,28 +67,34 @@ func (tcs timeChannels) toJSON() error {
 }
 
 type timeChannel struct {
-	Name       string
-	Timepoints timePoints
-	Output     bool
+	Name       string     `json:"name"`
+	Timepoints timePoints `json:"timepoints"`
+	output     bool
 }
 
-func (tc timeChannel) toJSON() error {
-	fmt.Println(tc)
-	b, err := json.Marshal(tc)
-	fmt.Println(b)
+func newtimeChannel(name string) *timeChannel {
+	return &timeChannel{Name: name}
+}
 
-	if err != nil {
-		return err
+func (tc *timeChannel) checkstate(timeNow time.Time) error {
+	daySecondNow := daySecond(timeNow.Hour(), timeNow.Minute(), timeNow.Second())
+	var thisWeekDayTimePoints timePoints
+	for _, timepoint := range tc.Timepoints {
+		if timepoint.WeekDay == timeNow.Weekday() {
+			thisWeekDayTimePoints = append(thisWeekDayTimePoints, timepoint)
+		}
 	}
-	f, err := os.OpenFile("test", os.O_CREATE|os.O_WRONLY, 0777)
-	//f, err := os.Open(fmt.Sprintf("%v", tc.name))
-	if err != nil {
-		return err
+	sort.Sort(thisWeekDayTimePoints)
+	for index, timePoint := range thisWeekDayTimePoints {
+		//  fmt.Printf("%v > %v \n", timePoint.getDaySecond(), daySecondNow)
+		if timePoint.getDaySecond() > daySecondNow {
+			tc.output = thisWeekDayTimePoints[index-1].State
+			return nil
+		}
 	}
-	defer f.Close()
-	_, err = f.Write(b)
-	if err != nil {
-		return err
+	if thisWeekDayTimePoints[len(thisWeekDayTimePoints)-1].getDaySecond() < daySecondNow {
+		tc.output = thisWeekDayTimePoints[len(thisWeekDayTimePoints)-1].State
+		return nil
 	}
 	return nil
 }
@@ -118,7 +116,7 @@ func (tp *timePoints) newtimePoint(weekDay, hour, minute, second int, state bool
 	})
 }
 
-func (tp timePoints) checkstate(timeNow time.Time) (bool, error) {
+/*func (tp timePoints) checkstate(timeNow time.Time) (bool, error) {
 	//timeNow := time.Now()
 	daySecondNow := daySecond(timeNow.Hour(), timeNow.Minute(), timeNow.Second())
 	var thisWeekDayTimePoints timePoints
@@ -138,7 +136,7 @@ func (tp timePoints) checkstate(timeNow time.Time) (bool, error) {
 		return thisWeekDayTimePoints[len(thisWeekDayTimePoints)-1].State, nil
 	}
 	return false, nil
-}
+}*/
 
 type timePoint struct {
 	//WeekDay time.Weekday "json:'weekday'"
